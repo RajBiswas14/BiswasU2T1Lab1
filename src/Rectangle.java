1ff80cf3b73f38bba680403515182606cd852ac0
1E10 laptop <sample@bths.edu>
2022-09-23 10:47:46 -0400

    u

---
+++ src/Rectangle.java
@@ -0,0 +1,28 @@
+public class Rectangle {
+
+    // Rectangle class definition
+    public class rectangle {
+
+        // instance variables
+        private int length;
+        private int width;
+
+        // constructor method for creating Rectangle objects;
+        // instance variables are set here using the values passed as arguments
+        public rectangle(int len, int wid) {
+            length = len;
+            width = wid;
+        }
+
+        // method that calculates and prints area
+        public void printArea() {
+            int area = length * width;
+            System.out.println("My area is " + area);
+        Public void printPerimeter() {
+         int perimeter = length + width;
+         System.out.print ln ("My perimeter is: " + perimeter);
+        }
+        }
+    }
+
+}
